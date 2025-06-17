eabb8c1a9d1d99561a2666aa2a4fa6b4e84c6066
twichai <twichai4869@gmail.com>
2025-06-17 18:42:06 +0700

    change harpoon key

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -1,43 +1,61 @@
 return {
-  "ThePrimeagen/harpoon",
-  branch = "harpoon2",
-  opts = {
-    menu = {
-      width = vim.api.nvim_win_get_width(0) - 4,
-    },
-    settings = {
-      save_on_toggle = true,
-    },
-  },
-  keys = function()
-    local keys = {
-      {
-        "<leader>H",
-        function()
-          require("harpoon"):list():add()
-        end,
-        desc = "Harpoon File",
-      },
-      {
-        "<leader>h",
-        function()
-          local harpoon = require("harpoon")
-          harpoon.ui:toggle_quick_menu(harpoon:list())
-        end,
-        desc = "Harpoon Quick Menu",
-      },
-    }
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	opts = {
+		menu = {
+			width = vim.api.nvim_win_get_width(0) - 4,
+		},
+		settings = {
+			save_on_toggle = true,
+		},
+	},
+	keys = function()
+		local harpoon = require("harpoon")
+		local keys = {
+			{
+				"<leader>H",
+				function()
+					harpoon:list():add()
+				end,
+				desc = "Harpoon Add File",
+			},
+			{
+				"<leader>h",
+				function()
+					harpoon.ui:toggle_quick_menu(harpoon:list())
+				end,
+				desc = "Harpoon Quick Menu",
+			},
+			{
+				"<leader>m",
+				function()
+					harpoon:list():select(1)
+				end,
+				desc = "Harpoon to File 1",
+			},
+			{
+				"<leader>,",
+				function()
+					harpoon:list():select(2)
+				end,
+				desc = "Harpoon to File 2",
+			},
+			{
+				"<leader>.",
+				function()
+					harpoon:list():select(3)
+				end,
+				desc = "Harpoon to File 3",
+			},
+			{
+				"<leader>/",
+				function()
+					harpoon:list():select(4)
+				end,
+				desc = "Harpoon to File 4",
+			},
+		}
 
-    name = {'m', ',', '.', '/'}
-    for i = 1, 5 do
-      table.insert(keys, {
-        "<leader>" .. i,
-        function()
-          require("harpoon"):list():select(i)
-        end,
-        desc = "Harpoon to File " .. i,
-      })
-    end
-    return keys
-  end,
+		return keys
+	end,
 }
